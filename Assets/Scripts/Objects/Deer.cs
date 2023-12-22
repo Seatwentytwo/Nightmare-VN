89e643224508426266b92dc34b63bf3f78358c8b
Seatwentytwo <C22480762@mytudublin.ie>
2023-12-22 04:00:59 +0000

    Heading to sleep now
    ZZZzzz...

--- Assets/Scripts/Objects/Deer.cs
+++ Assets/Scripts/Objects/Deer.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Deer : MonoBehaviour
 {
+    bool PlayerDetected;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        PlayerDetected = false;
+        animator.SetBool("PlayerDetected", PlayerDetected);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerDetected = GameObject.Find("Deer Detect").GetComponent<Deer_Detection>().PlayerDetected;
     }
 }
---
+++ Assets/Scripts/Objects/Meat_Collect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meat_Collect : MonoBehaviour
+{
+    public bool MeatCollected;
+    Renderer rend;
+    Collider2D collide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeatCollected = false;
+        rend = GetComponent<Renderer>();
+        collide = GetComponent<Collider2D>();
+
+        rend.enabled = true;
+        collide.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        MeatCollected = true;
+        rend.enabled = false;
+        collide.enabled = false;
+    }
+}
