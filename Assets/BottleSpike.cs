6119c4f224a33a8517244d08b1730e3e23da7d38
Seatwentytwo <C22480762@mytudublin.ie>
2023-12-22 19:53:46 +0000

    Touch Ups

--- Assets/BottleSpike.cs
+++ Assets/BottleSpike.cs
@@ -6,23 +6,26 @@ public class BottleSpike : MonoBehaviour
 {
     bool PillsCollected;
     public bool SpikePunch;
+    bool Punch_Collected;
 
     // Start is called before the first frame update
     void Start()
     {
         PillsCollected = false;
         SpikePunch = false;
+        Punch_Collected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         PillsCollected = GameObject.Find("Pill").GetComponent<Pills_Collect>().PillsCollected;
+        Punch_Collected = GameObject.Find("Punch").GetComponent<Punch_Collect>().PunchCollected;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (PillsCollected == true)
+        if (PillsCollected == true && Punch_Collected == true)
         {
             SpikePunch = true;
         }
---
+++ Assets/Punch_Collect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch_Collect : MonoBehaviour
+{
+    public bool PunchCollected;
+    Renderer rend;
+    Collider2D collide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PunchCollected = false;
+        rend = GetComponent<Renderer>();
+        collide = GetComponent<Collider2D>();
+
+        rend.enabled = true;
+        collide.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PunchCollected = true;
+        rend.enabled = false;
+        collide.enabled = false;
+    }
+}
