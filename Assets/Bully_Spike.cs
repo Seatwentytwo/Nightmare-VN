1d320be9945e98874cc4784582ff791454dba4b6
Seatwentytwo <C22480762@mytudublin.ie>
2023-12-22 22:37:21 +0000

    dfgd

--- Assets/Bully_Spike.cs
+++ Assets/Bully_Spike.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Bully_Spike : MonoBehaviour
 {
     bool Spiked;
+    bool Punch;
     Collider2D collide;
 
     // Start is called before the first frame update
     void Start()
     {
         Spiked = false;
+        Punch = false;
         collide = GetComponent<Collider2D>();
 
         collide.enabled = true;
@@ -19,12 +21,13 @@ public class Bully_Spike : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Spiked = GameObject.Find("Bottle").GetComponent<BottleSpike>().SpikePunch;
+        Spiked = GameObject.Find("Pill").GetComponent<Pills_Collect>().PillsCollected;
+        Punch = GameObject.Find("Punch").GetComponent<Punch_Collect>().PunchCollected;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Spiked == true)
+        if (Spiked == true && Punch == true)
         {
             collide.enabled = false;
         }
